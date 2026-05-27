43a40ccaa191c95f10c699e2ecaeb9dea25eab8d
flaz14 <>
2026-05-27 20:35:37 +0300

    Start writing the article

---
+++ collections-recursion/src/ConcurrentModificationExample.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class ConcurrentModificationExample {
+    public static void main(String[] args) {
+        Set<Integer> hashSet = new HashSet<>();
+        hashSet.add(41);
+        hashSet.add(19);
+        hashSet.add(43);
+        hashSet.add(13);
+
+        var iterator1 = hashSet.iterator();
+        var iterator2 = hashSet.iterator();
+
+        iterator1.next();
+        iterator2.next();
+        iterator2.remove();
+        iterator1.next();
+    }
+}
+
